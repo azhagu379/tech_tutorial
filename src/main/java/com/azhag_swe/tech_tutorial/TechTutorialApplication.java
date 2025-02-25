5564e1d9de061d493fb2026bcc81da050fc7d004
azhagu-swe <azhagu.swe@gmail.com>
2025-02-25 09:09:31 +0530

    first commit

---
+++ src/main/java/com/azhag_swe/tech_tutorial/TechTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.azhag_swe.tech_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azhag_swe/tech_tutorial/TechTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azhag_swe.tech_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
